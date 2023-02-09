44c3b8a5135f5b0425ccd7e20b3d03edd5c57a9e
jahidanowar <milon5143@gmail.com>
2023-02-09 19:34:18 +0530

    fix: Mongoose Dependecy Issue

--- server/api/product/[id]/index.get.ts
+++ server/api/product/[id]/index.get.ts
@@ -1,37 +1,46 @@
-import Product from '~/server/models/Product.model';
+import Product from "~/server/models/Product.model";
+import Option from "~~/server/models/Option.model";
+import Toughbook from "~~/server/models/Toughbook.model";
 
 export default defineEventHandler(async (event) => {
-	try {
-		const { id } = event.context.params;
+  try {
+    // const { id } = event.context.params; // Never extract params like this instead use the following
+    const { id } = getRouterParams(event);
 
-		const product = await Product.findOne({ _id: id }).populate(
-			'toughbooks options'
-		);
+    const product = await Product.findOne({ _id: id })
+      .populate({
+        path: "options",
+        model: Option,
+      })
+      .populate({
+        path: "toughbooks",
+        model: Toughbook,
+      });
 
-		// const product = await Product.aggregate([
-		// 	{ $match: { name: 'Toughbook 55' } },
-		// 	{
-		// 		$group: {
-		// 			_id: null,
-		// 			data: { $first: '$$ROOT' },
-		// 		},
-		// 	},
-		// 	{
-		// 		$lookup: {
-		// 			from: 'data.toughbooks',
-		// 			localField: 'toughbooks',
-		// 			foreignField: '_id',
-		// 			as: 'models',
-		// 		},
-		// 	},
-		// ]);
+    // const product = await Product.aggregate([
+    // 	{ $match: { name: 'Toughbook 55' } },
+    // 	{
+    // 		$group: {
+    // 			_id: null,
+    // 			data: { $first: '$$ROOT' },
+    // 		},
+    // 	},
+    // 	{
+    // 		$lookup: {
+    // 			from: 'data.toughbooks',
+    // 			localField: 'toughbooks',
+    // 			foreignField: '_id',
+    // 			as: 'models',
+    // 		},
+    // 	},
+    // ]);
 
-		// console.log(product);
+    // console.log(product);
 
-		return product;
+    return product;
 
-		// const product = await Product.findOne({ _id: id });
-	} catch (error) {
-		console.log(error);
-	}
+    // const product = await Product.findOne({ _id: id });
+  } catch (error) {
+    console.log(error);
+  }
 });
